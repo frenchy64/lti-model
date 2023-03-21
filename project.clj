ee1f142d7277841d55ab8a4538d8e37777b2ef94
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2023-03-21 17:11:26 -0400

    build

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ; 2gb stack size
   :jvm-opts ["-Xss1g"]
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [dorothy "0.0.7"]
                  [hiccup "1.0.5"]])
